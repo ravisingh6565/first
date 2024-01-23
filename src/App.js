a1d8181f2d0b38a99800c501899e682c3e3d0eec
ravisingh6565 <ravisuneel6262@gmail.com>
2024-01-23 23:45:28 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import {Header,Footer} from "./Header";
 
 function App() {
   return (
     <div className="App">
+    <Header/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
           <h1>react js code is load</h1>
         </p>
         <a
@@ -16,9 +18,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {/* Learn React */}
         </a>
       </header>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,3 @@
+const Header = ()=><h1>Header of the web app</h1>
+const Footer = ()=><h1>Hello is a Footer</h1>
+export  {Header,Footer};
